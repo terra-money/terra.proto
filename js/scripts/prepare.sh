a12445f30c002e46befb766b6aa3b40392b9b907
emidev98 <emilian@terra.money>
2023-02-23 11:02:14 +0100

    fix: stride proto gen files path on publish

--- js/scripts/prepare.sh
+++ js/scripts/prepare.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-DIRS="alliance confio cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint"
+DIRS="stride alliance confio cosmos cosmos_proto cosmwasm gogoproto google ibc tendermint"
 
 for dir in $DIRS; do
   rm -rf "$dir"
