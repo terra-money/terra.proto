5196d3850f598df77a7c8a17617a9c95f65a789f
emidev98 <emilian@terra.money>
2023-09-17 12:58:18 +0800

    fix: protobuf permissions

--- js/scripts/proto-gen.sh
+++ js/scripts/proto-gen.sh
@@ -10,23 +10,29 @@ PROTOC_GEN_TS_PROTO_PATH="./node_modules/.bin/protoc-gen-ts_proto"
 mkdir -p "$OUT_DIR"
 
 echo "Processing proto files ..."
+PROTOBUF_DIR="../protobuf"
 COSMOS_SDK_DIR="../cosmos-sdk/proto"
-COSMOS_SDK_THIRD_PARTY_DIR="../cosmos-sdk/third_party/proto"
-IBC_DIR="../ibc-go/proto"
-WASMD_DIR="../wasmd/proto"
 ALLIANCE_DIR="../alliance/proto"
+IBC_DIR="../ibc-go/proto"
+PFM_DIR="../ibc-apps/middleware/packet-forward-middleware/proto"
+WASMD_DIR="../wasmd/proto"
+GRPC_DIR="../grpc-gateway"
 JAX_DIR="../jax/proto"
-THIRD_PARTY="../third_party"
+COSMOS_DIR="../cosmos-proto/proto"
+TERRA_DIR="../terra/proto"
 
 protoc \
   --plugin="protoc-gen-ts_proto=${PROTOC_GEN_TS_PROTO_PATH}" \
   --ts_proto_out="${OUT_DIR}" \
   --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true,outputClientImpl=grpc-web" \
+  --proto_path="$PROTOBUF_DIR" \
   --proto_path="$COSMOS_SDK_DIR" \
-  --proto_path="$COSMOS_SDK_THIRD_PARTY_DIR" \
-  --proto_path="$WASMD_DIR" \
-  --proto_path="$IBC_DIR" \
   --proto_path="$ALLIANCE_DIR" \
+  --proto_path="$IBC_DIR" \
+  --proto_path="$PFM_DIR" \
+  --proto_path="$WASMD_DIR" \
+  --proto_path="$GRPC_DIR" \
   --proto_path="$JAX_DIR" \
-  --proto_path="$THIRD_PARTY" \
-  $(find ${COSMOS_SDK_DIR} ${COSMOS_SDK_THIRD_PARTY_DIR} ${WASMD_DIR} ${IBC_DIR} ${ALLIANCE_DIR} ${JAX_DIR} ${THIRD_PARTY} -path -prune -o -name '*.proto' -print0 | xargs -0)
+  --proto_path="$COSMOS_DIR" \
+  --proto_path="$TERRA_DIR" \
+  $(find  $PROTOBUF_DIR $COSMOS_SDK_DIR $ALLIANCE_DIR $IBC_DIR $PFM_DIR $WASMD_DIR $GRPC_DIR $JAX_DIR $COSMOS_DIR $TERRA_DIR -path -prune -o -name '*.proto' -print0 | xargs -0)
